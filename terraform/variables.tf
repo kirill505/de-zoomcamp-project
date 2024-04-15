58d91de39eeeffa98dcd14b1149b6ddb29652f14
Kirill Bazarov <kl.bazarov@gmail.com>
2024-04-15 10:59:28 -0300

    terraform: deploying mage

---
+++ terraform/variables.tf
@@ -0,0 +1,69 @@
+variable "app_name" {
+  type        = string
+  description = "Application Name"
+  default     = "de-final-project"
+}
+
+variable "container_cpu" {
+  description = "Container cpu"
+  default     = "2000m"
+}
+
+variable "container_memory" {
+  description = "Container memory"
+  default     = "2G"
+}
+
+variable "project_id" {
+  type        = string
+  description = "The name of the project"
+  default     = "de-zoomcamp-project-ph"
+}
+
+variable "region" {
+  type        = string
+  description = "The default compute region"
+  default     = "us-west2"
+}
+
+variable "zone" {
+  type        = string
+  description = "The default compute zone"
+  default     = "us-west2-a"
+}
+
+variable "repository" {
+  type        = string
+  description = "The name of the Artifact Registry repository to be created"
+  default     = "de-final-project"
+}
+
+variable "database_user" {
+  type        = string
+  description = "The username of the Postgres database."
+  default     = "mageuser"
+}
+
+variable "database_password" {
+  type        = string
+  description = "The password of the Postgres database."
+  sensitive   = true
+}
+
+variable "docker_image" {
+  type        = string
+  description = "The docker image to deploy to Cloud Run."
+  default     = "mageai/mageai:latest"
+}
+
+variable "domain" {
+  description = "Domain name to run the load balancer on. Used if `ssl` is `true`."
+  type        = string
+  default     = ""
+}
+
+variable "ssl" {
+  description = "Run load balancer on HTTPS and provision managed certificate with provided `domain`."
+  type        = bool
+  default     = false
+}

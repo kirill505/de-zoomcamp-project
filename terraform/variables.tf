bb6ffddc3f1eab1fc10dee499ce5664d9ec3f6bb
Kirill Bazarov <kl.bazarov@gmail.com>
2024-04-24 14:33:51 -0300

    Terraform: downgrade to more simple IaC

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,69 +1,41 @@
-variable "app_name" {
-  type        = string
-  description = "Application Name"
-  default     = "de-final-project"
+variable "credentials" {
+  description = "My Credentials"
+  default     = "<Path to your Service Account json file>"
+  #ex: if you have a directory where this file is called keys with your service account json file
+  #saved there as my-creds.json you could use default = "./keys/my-creds.json"
 }
 
-variable "container_cpu" {
-  description = "Container cpu"
-  default     = "2000m"
-}
-
-variable "container_memory" {
-  description = "Container memory"
-  default     = "2G"
-}
 
 variable "project_id" {
-  type        = string
-  description = "The name of the project"
+  description = "The Project ID"
   default     = "de-zoomcamp-project-ph"
 }
 
 variable "region" {
-  type        = string
-  description = "The default compute region"
-  default     = "us-west2"
+  description = "Region"
+  #Update the below to your desired region
+  default = "us-west1"
 }
 
-variable "zone" {
-  type        = string
-  description = "The default compute zone"
-  default     = "us-west2-a"
+variable "location" {
+  description = "Project Location"
+  #Update the below to your desired location
+  default = "US"
 }
 
-variable "repository" {
-  type        = string
-  description = "The name of the Artifact Registry repository to be created"
-  default     = "de-final-project"
+variable "bq_dataset_name" {
+  description = "My BigQuery Dataset Name"
+  #Update the below to what you want your dataset to be called
+  default = "de_zoomcamp_project_dev"
 }
 
-variable "database_user" {
-  type        = string
-  description = "The username of the Postgres database."
-  default     = "mageuser"
+variable "gcs_bucket_name" {
+  description = "My Storage Bucket Name"
+  #Update the below to a unique bucket name
+  default = "de-zoomcamp-project-ph-dev"
 }
 
-variable "database_password" {
-  type        = string
-  description = "The password of the Postgres database."
-  sensitive   = true
-}
-
-variable "docker_image" {
-  type        = string
-  description = "The docker image to deploy to Cloud Run."
-  default     = "mageai/mageai:latest"
-}
-
-variable "domain" {
-  description = "Domain name to run the load balancer on. Used if `ssl` is `true`."
-  type        = string
-  default     = ""
-}
-
-variable "ssl" {
-  description = "Run load balancer on HTTPS and provision managed certificate with provided `domain`."
-  type        = bool
-  default     = false
+variable "gcs_storage_class" {
+  description = "Bucket Storage Class"
+  default     = "STANDARD"
 }
